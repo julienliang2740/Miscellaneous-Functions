95e4f6a9e0843f5c0c2b3a8ca6abef24c253a857
julienliang2740 <88892980+julienliang2740@users.noreply.github.com>
2022-10-20 09:29:49 -0400

    Update fizz-buzz.rkt

--- fizz-buzz.rkt
+++ fizz-buzz.rkt
@@ -1,13 +1,6 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname fizz-buzz) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
-;;
-;; ***************************************************
-;; Julien Liang (21006564)
-;; CS 135 Fall 2022
-;; Assignment 04, Problem 5
-;; ***************************************************
-;;
 
 ;; (fizz-buzz start end fizz buzz) consumes a start integer, end integer, fizz integer,
 ;; and buzz integer and produces a list from the start integer to the end integer where
