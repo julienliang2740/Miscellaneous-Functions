331d77cbe8fd3d3fb8e15cc3c305a6ebbfdda7a1
julienliang2740 <88892980+julienliang2740@users.noreply.github.com>
2022-10-20 09:30:45 -0400

    Update range.rkt

--- range.rkt
+++ range.rkt
@@ -1,13 +1,6 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname range) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
-;;
-;; ***************************************************
-;; Julien Liang (21006564)
-;; CS 135 Fall 2022
-;; Assignment 04, Problem 2
-;; ***************************************************
-;;
 
 ;;
 ;; a)
